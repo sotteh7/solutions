005669fc6e9100db402df362bb7f72e92afaa9e7
git-leon <xleonhunter@gmail.com>
2020-08-11 14:57:14 -0400

    update

--- maven.streams-and-lambdas/src/main/java/com/github/curriculeon/anthropoid/PersonFactory.java
+++ maven.streams-and-lambdas/src/main/java/com/github/curriculeon/anthropoid/PersonFactory.java
@@ -5,6 +5,7 @@ import com.github.curriculeon.tools.StringUtils;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -21,7 +22,7 @@ public final class PersonFactory {
      * @return a new instance of a person with fields of random values
      */
     public Person createRandomPerson() {
-        String name = StringUtils.capitalizeFirstChar(RandomUtils.createString('a', 'e', 3));
+        String name = StringUtils.capitalizeFirstChar(RandomUtils.createString('a', 'e', 5));
         String[] aliases = RandomUtils.createStrings('a', 'z', 3, 5);
         boolean isMale = RandomUtils.createBoolean(50);
         long personalId = System.nanoTime();
@@ -32,13 +33,25 @@ public final class PersonFactory {
     }
 
     /**
+     * Section 8.2
+     *
+     * @param streamCount - number of Person objects to create
+     * @return - Stream representation of collection of Person objects
+     */ // TODO
+    public Stream<Person> createPersonStream(int streamCount) {
+        return Stream
+                .generate(this::createRandomPerson)
+                .limit(streamCount);
+    }
+
+    /**
      * Section 8.8
      *
      * @param listSize - number of Person objects to create
      * @return - ArrayList of Person objects
      */ // TODO
     public List<Person> createPersonList(int listSize) {
-        return null;
+        return createPersonStream(listSize).collect(Collectors.toList());
     }
 
 
@@ -47,17 +60,7 @@ public final class PersonFactory {
      * @return - Array of Person objects
      */ // TODO
     public Person[] createPersonArray(int arrayLength) {
-        return null;
+        return createPersonList(arrayLength).toArray(new Person[arrayLength]);
     }
 
-
-    /**
-     * Section 8.2
-     *
-     * @param streamCount - number of Person objects to create
-     * @return - Stream representation of collection of Person objects
-     */ // TODO
-    public Stream<Person> createPersonStream(int streamCount) {
-        return null;
-    }
 }
